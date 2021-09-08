febe45d54dcc4e53ba21b9b93fedda1ef4a0bd3b
smahli <mahlishiwshankar@gmail.com>
2021-09-08 16:37:00 -0700

    just basic

---
+++ BasicProgram/Variables.java
@@ -0,0 +1,14 @@
+package BasicProgram;
+
+public class Variables {
+    public static void main(String[] args) {
+        // Numeric data types
+        // short stores two bytes
+        //Integer stores four bytes
+
+        short a =4;
+        Integer  myNum = 5;
+        System.out.println(a);
+        System.out.println(myNum);
+    }
+}
