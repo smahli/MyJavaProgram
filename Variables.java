6479a494db49975a97506918f79960382cfaa604
smahli <mahlishiwshankar@gmail.com>
2021-09-08 16:27:39 -0700

    def of var and some basic program

---
+++ Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myAge =25;
+        int myNumber =1996;
+
+        System.out.println(myAge);
+        System.out.println(myNumber);
+    }
+    
+}
