a2b9ed3728da59081f4a4a29609ddf348b21a471
smahli <mahlishiwshankar@gmail.com>
2021-09-09 21:54:46 -0700

    init

---
+++ BankingApps/Methods/SourceCode.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void greetings(){
+        System.out.println("Hello There Good Morning! How you doing");
+    }
+
+    public static void public static void main(String[] args) {
+        
+    }
+}
