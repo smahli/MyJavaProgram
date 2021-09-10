f13e0427c590e1923773c6d95ba06edd3c0dd3eb
smahli <mahlishiwshankar@gmail.com>
2021-09-10 16:40:25 -0700

    nicely working

---
+++ BasicProgram/pattern7.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class pattern7 {
+    public static void main(String[] args) {
+        
+       
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter how long you want to print");
+        int n = sc.nextInt();
+
+      
+
+        for( int i=1;i<=n;i++){
+            for( int j=n;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print(" *");
+            }
+            System.out.println();
+
+    }
+    
+}
+}
