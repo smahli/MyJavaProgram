0fc6335d53f7d576285fd13fe4f3fbc78145903c
smahli <mahlishiwshankar@gmail.com>
2021-09-10 15:11:48 -0700

    works perfectly

--- BasicProgram/Pattern4.java
+++ BasicProgram/Pattern4.java
@@ -8,12 +8,12 @@ public class Pattern4 {
         System.out.println(" How long you want print");
         int n = sc.nextInt();
 
-        for(int i =1; i<=n-1;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("");
+        for(int i =1; i<=n;i++){
+            for(int j=n-1;j>=i;j--){
+                System.out.print(" ");
             }
-            System.out.println();
-            for(int k=1;k<=n;k++){
+            
+            for(int k=1;k<=i;k++){
                 System.out.print("*");
             }
             System.out.println();
