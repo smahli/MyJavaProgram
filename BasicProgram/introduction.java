b26a5adbd6967ce2eb3d2e6c1709c6b543d5a4eb
smahli <mahlishiwshankar@gmail.com>
2021-09-11 12:56:30 -0700

    done

--- BasicProgram/introduction.java
+++ BasicProgram/introduction.java
@@ -3,20 +3,24 @@ import java.util.Scanner;
 public class introduction {
     public static void main(String[] args) {
         Scanner cs = new Scanner(System.in);
+
         System.out.println("Enter your name");
-        String name= cs.nextLine();
+        String name=cs.next();
         System.out.println("Enter your age ");
-        int n= cs.nextInt();
+        int age= cs.nextInt();
         System.out.println(" Enter your Gender");
-        String gender = cs.nextLine();
+        char gender = cs.next().charAt(0);
         System.out.println(" Enter your school name");
-        String school = cs.nextLine();
+        String school = cs.next();
+        System.out.println(" Enter your mobile no");
+        long mobno= cs.nextLong();
 
         System.out.println(" Your Introduction ");
         System.out.println(" Your name "+ name);
-        System.out.println(" Your age :"+ n);
+        System.out.println(" Your age :"+ age);
         System.out.println(" Your Gender :"+ gender);
         System.out.println(" Your School name :"+ school);
+        System.out.println(" Your mobile  no :"+ mobno);
 
     }
     
