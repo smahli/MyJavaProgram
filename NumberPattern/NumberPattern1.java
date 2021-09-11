6354fc070ff680d2d3c7c2116a46453b44f083d4
smahli <mahlishiwshankar@gmail.com>
2021-09-10 23:07:45 -0700

    some correction needed

---
+++ NumberPattern/NumberPattern1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class NumberPattern1 {
+    public static void main(String[] args) {
+        Scanner cs = new Scanner(System.in);
+
+
+        System.out.println(" Enter a number how long you want to print");
+        int n= cs.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+                i=i+1;
+            }
+            
+            System.out.println();
+        }
+    }
+    
+}
