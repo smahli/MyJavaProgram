b4a1df421db200e1d45d5adde9c8fb3815ec7bd5
smahli <mahlishiwshankar@gmail.com>
2021-09-12 11:54:00 -0700

    initial commit

--- BasicProgram/Armstrong.java
+++ BasicProgram/Armstrong.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Armstrong {
     public static void main(String[] args) {
+        Scanner sc =new Scanner(system.in);
+        System.out.println("Enter a number to check Palindrome :");
+        int n= sc.nextInt();
         
     }
     
