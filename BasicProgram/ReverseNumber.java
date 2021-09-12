aa9f77bd42258434b5ee52ecf645c83193e05ab7
smahli <mahlishiwshankar@gmail.com>
2021-09-11 23:22:30 -0700

    working

--- BasicProgram/ReverseNumber.java
+++ BasicProgram/ReverseNumber.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class ReverseNumber {
     
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Enter a Number you want to reverse");
+        long n = sc.nextLong();
+        long reverse =0;
         
+        while(n !=0){
+            long rem = n % 10;
+             reverse = reverse * 10 + rem;
+             n= n/10;
+
+        }
+        System.out.println("Reverse No:"+ reverse);
     }
 }
