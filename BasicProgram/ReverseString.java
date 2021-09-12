51d78125b2698834ede52f261d12ef320ab6f8c2
smahli <mahlishiwshankar@gmail.com>
2021-09-11 23:46:39 -0700

    works

--- BasicProgram/ReverseString.java
+++ BasicProgram/ReverseString.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class ReverseString {
     
     public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println(" Enter a String to reverse");
+        String name = sc.next();
+        String reverse = "";
+        int l = name.length();
+        for( int i=l-1;i>=0;i--){
+            reverse = reverse + name.charAt(i);
+
+
+        }
+        System.out.println("Reverse String:"+ reverse);
         
     }
 }
