14d67e6630eb03ff0307c526954dd0515f58f6da
smahli <mahlishiwshankar@gmail.com>
2021-09-12 12:12:34 -0700

    working

--- BasicProgram/Armstrong.java
+++ BasicProgram/Armstrong.java
@@ -2,9 +2,7 @@ import java.util.Scanner;
 
 public class Armstrong {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(system.in);
-        System.out.println("Enter a number to check Palindrome :");
-        int n= sc.nextInt();
+        
         
     }
     
--- BasicProgram/Palindrome.java
+++ BasicProgram/Palindrome.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 public class Palindrome {
     public static void main(String[] args) {
+        long rev=0;
+        long rem;
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter a number to check Palindrome :");
+        long n = sc.nextLong();
+        long temp=n;
+        while(n!=0){
+            
+            
+            rem =(n%10);
+            rev=rev*10+rem;
+            n=n/10;
+
+        }
         
+        if(rev==temp){
+            System.out.println(" Number Is Palindrome");
+
+            
+        }
+        else{
+            System.out.println(" No is not Palindrome");
+        }
+
     }
     
 }
