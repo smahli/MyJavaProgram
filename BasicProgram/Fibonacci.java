fce8b4054b44ed515ff1ceb4167dc8dfd53e9856
smahli <mahlishiwshankar@gmail.com>
2021-09-12 10:57:51 -0700

    works

--- BasicProgram/Fibonacci.java
+++ BasicProgram/Fibonacci.java
@@ -1,5 +1,44 @@
 public class Fibonacci{
     public static void main(String[] args) {
         
+
+
+
+
+        import java.util.Scanner;
+
+public class Prime2 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter How long you want Prime no");
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            int temp=0;
+            for(int j=2;j<=i-1;j++){
+                if(i%j==0){
+                    temp=temp+1;
+                }
+                else{
+                    temp=0;
+                }
+               
+            }
+            if(temp==0){
+                System.out.println("Prime No: "+ i);
+
+            }
+            else{
+                temp=temp+1;
+            }
+
+        }
+        
+    }
+
+
+
+    
+}
+
     }
 }
--- BasicProgram/Prime2.java
+++ BasicProgram/Prime2.java
@@ -1,35 +1,25 @@
 import java.util.Scanner;
 
 public class Prime2 {
-   
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a Number to check prime");
-        int n = sc.nextInt();
-        
-        for(int j=1;j<=n;j++){
-            int temp=0;
-            for(int i=2;i<=n-1;i++){
-                if(n%i==0){
-                    temp=temp+1;
-                }
-            
-    
-    
-    
-            }
-            
-            if(temp==0){
-                System.out.println(" No is Prime"+ j);;
-    
-            }
-            else{
-               temp= temp+1;
-            }
-
+public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+    System.out.println("Enter How long you want Prime no");
+    int n = sc.nextInt();
+    for(int j =1;j<=n;j++){
+        int temp=0;
+        for(int i=2;i<=j-1;i++){
+           if(j%i==0){
+               temp=temp+1;
+           }
         }
-       
-        
+        if(temp==0){
+            System.out.println(" prime:      " + j);
+        }
+        else{
+            
+            temp=temp+1;
+        }
     }
     
+}
 }
