2313aa6805b8074833b3dde309cc3a2108e3595c
smahli <mahlishiwshankar@gmail.com>
2021-09-11 23:59:12 -0700

    works

--- BasicProgram/Prime.java
+++ BasicProgram/Prime.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Prime {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number to check prime");
+        int n = sc.nextInt();
+        int temp=0;
+        for(int i=2;i<=n-1;i++){
+            if(n%i==0){
+                temp=temp+1;
+            }
+        
+
+
+
+        }
+        
+        if(temp>0){
+            System.out.println(" No is Not Prime");;
+
+        }
+        else{
+            System.out.println("No Is Prime");
+        }
         
     }
     
