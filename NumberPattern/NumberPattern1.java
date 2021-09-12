052056cd151f6807f3f54b918bb4e112cce2486d
smahli <mahlishiwshankar@gmail.com>
2021-09-12 13:09:02 -0700

    working fine

--- NumberPattern/NumberPattern1.java
+++ NumberPattern/NumberPattern1.java
@@ -11,7 +11,7 @@ public class NumberPattern1 {
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(i);
-                i=i+1;
+        
             }
             
             System.out.println();
