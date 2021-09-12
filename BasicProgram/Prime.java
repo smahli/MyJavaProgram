bb3379c6c357d307a61c1911e42760bc33b93d93
smahli <mahlishiwshankar@gmail.com>
2021-09-11 23:48:46 -0700

    initial commit

--- BasicProgram/Prime.java
+++ BasicProgram/Prime.java
@@ -1,21 +1,6 @@
 public class Prime {
-    // Find Prime no from 2 to 1000
-
-    public static boolean IsPrime(int number){
-
-
-        for(int i=2;i<= number;i++){
-
-
-            
-        }
-    }
-
-
-
     public static void main(String[] args) {
         
-
     }
     
 }
