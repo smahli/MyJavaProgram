f0e3ac2fa22415462fd5d8e8be912089b1690464
smahli <mahlishiwshankar@gmail.com>
2021-09-12 00:10:33 -0700

    initial commit

--- BasicProgram/Prime2.java
+++ BasicProgram/Prime2.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Prime2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("how long you want prime numbers from 1");
+        int n = sc.nextInt();
+        int temp=0;
+        for( int j=1;j<=n;j++){
+            for(int i=2;i<=j-1;i++){
+                if(n%i==0){
+                    temp=temp+1;
+                }
+            
+                if(temp==0){
+                    System.out.println(" Prime No: " + j);
+                }
+    
+    
+            }
+            
+            
+
+        }
+    
         
     }
     
