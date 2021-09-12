cff7464f207277ab50e79b16927bb552f9740c02
smahli <mahlishiwshankar@gmail.com>
2021-09-12 15:17:38 -0700

    just introduction

---
+++ ArrayProblems/Array1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Array1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" enter size of the array");
+        int n = sc.nextInt();
+       /* int[]a;
+        a= new int[n];*/
+
+        int a[] = new int[n];
+       
+       /* a[0]=10;
+        a[1]=20;
+        a[2]=30;*/
+        for(i=0;i<a.length;i++){
+            System.out.println(" Enter your roll no:");
+
+        }
+    }
+    
+}
