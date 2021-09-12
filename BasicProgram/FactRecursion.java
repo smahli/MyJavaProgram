38915a614bf5c3fcb010211b40ff3e89f9475cab
smahli <mahlishiwshankar@gmail.com>
2021-09-11 22:59:03 -0700

    working good

--- BasicProgram/FactRecursion.java
+++ BasicProgram/FactRecursion.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
 public class FactRecursion {
+    int fact =1;
     public static void main(String[] args) {
+       
+        int result;
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter a Number you want to calculate factorial");
+        int n=sc.nextInt();
+        FactRecursion f = new FactRecursion();
+        result = f.calcFact(n);
+        System.out.println("Factorial of "+ n + " is  "+ result);
         
+    }
+    int calcFact(int a ){
+    
+        if(a>1){
+            fact = fact * a;
+            calcFact(a-1);
+            
+
+
+        }
+        else if(a==0){
+            return fact=0;
+        }
+        return fact;
+
     }
     
 }
