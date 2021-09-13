086352435707e68dd53093f2f2bce2c39aa235cf
smahli <mahlishiwshankar@gmail.com>
2021-09-12 22:14:34 -0700

    initial commit

---
+++ ArrayProblems/MinElement.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class MinElement {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter index of matrix");
+        int n = sc.nextInt();
+        int a[]= new int[n];
+        
+        System.out.println("Enter elements");
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        int max = a[0];
+        for(int j=1;j<n;j++){
+            if(a[j] <=max){
+                max =a[j];
+            }
+        }
+        System.out.println("Min is "+max);
+    }
+}
+
+
+    
+
+    
+
